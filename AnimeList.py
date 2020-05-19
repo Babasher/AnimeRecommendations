955633c8232d568702791e7a96321caa2aedd3d6
Omar <omohammed7869@gmail.com>
2020-05-19 01:08:25 -0500

    created number_of_matches() function and updated recommended_list() function. Updating some variable names

--- AnimeList.py
+++ AnimeList.py
@@ -6,14 +6,13 @@ jikan = Jikan()
 
 
 def main():
-    #Test Code line 9 - 14
+    # Test Code line 10 - 33
     our_list = [{'Genres': ['Drama', 'Romance', 'School', 'Shoujo Ai'],
                  'Title': 'Citrus'},
                 {'Genres': ['Action', 'Adventure', 'Comedy', 'Super Power', 'Supernatural', 'Shounen'],
                  'Title': 'Bleach'},
                 {'Genres': ['Comedy', 'Ecchi', 'Romance', 'School', 'Seinen'], 'Title': 'Prison School'}]
 
-    # mushishi = jikan.anime(457, extension="pictures")
     top_anime = jikan.top(type="anime", page=1, subtype="upcoming")
     id_list = get_id(top_anime)
     upcoming_shows = get_anime_object(id_list)
@@ -22,12 +21,15 @@ def main():
 
     print('*************Watched Shows************')
     print_table(our_list)
+    print()
 
     print('*************Upcoming Shows************')
     print_table(upcoming_shows)
+    print()
 
     print('*************Recommended Based Off of Our List************')
     print_table(recommendations)
+    print()
 
 
 def get_id(data):
@@ -60,16 +62,16 @@ def get_anime_object(data):
     return anime_list
 
 
-def print_table(list):
+def print_table(list_of_anime):
     max_length = 0
-    for anime in list:
+    for anime in list_of_anime:
         current_length = len(anime['Title'])
         if current_length > max_length:
             max_length = current_length
 
     print("Title" + max_length*' ' + "Genre")
     spaces = 3 + max_length
-    for anime in list:
+    for anime in list_of_anime:
         distance = spaces - len(anime['Title'])
         print(anime['Title'], distance*' ', anime['Genres'])
 
@@ -79,17 +81,26 @@ def recommended_list(our_list, top_anime):
     for anime in our_list:
         genre_list.append(anime['Genres'])
 
-    flattened_list = [genre for item in genre_list for genre in item]
-    flattened_list = list(dict.fromkeys(flattened_list))
+    flattened_list = [genre for item in genre_list for genre in item]  # Removes nested lists
+    flattened_list = list(dict.fromkeys(flattened_list))  # Removes duplicate genres
 
     recommendation = []
-    flattened_list = set(flattened_list)
+    flattened_list = set(flattened_list)  # Creates a set
     for item in top_anime:
-        if set(item['Genres']) & set(flattened_list):
+        if set(item['Genres']) & flattened_list and\
+        (number_of_matches(set(item['Genres']), set(flattened_list)) > 1):  # If both lists intersect and have
+                                                                            # more than 1 similar genre
             recommendation.append(item)
 
     return recommendation
 
+
+def number_of_matches(list_one, list_two):
+    num = 0
+    for x in list_one:
+        if x in list_two:
+            num += 1
+    return num
 
 main()
 
