216011dcba73bcb4ed79f9db22ad1f5643ef5d36
Omar <omohammed7869@gmail.com>
2020-05-19 00:23:48 -0500

    Added working version of recommended_list() method

--- AnimeList.py
+++ AnimeList.py
@@ -6,19 +6,35 @@ jikan = Jikan()
 
 
 def main():
-    #mushishi = jikan.anime(457, extension="pictures")
-    top_anime = jikan.top(type="anime", page=1, subtype="upcoming")
+    #Test Code line 9 - 14
+    our_list = [{'Genres': ['Drama', 'Romance', 'School', 'Shoujo Ai'],
+                 'Title': 'Citrus'},
+                {'Genres': ['Action', 'Adventure', 'Comedy', 'Super Power', 'Supernatural', 'Shounen'],
+                 'Title': 'Bleach'},
+                {'Genres': ['Comedy', 'Ecchi', 'Romance', 'School', 'Seinen'], 'Title': 'Prison School'}]
 
+    # mushishi = jikan.anime(457, extension="pictures")
+    top_anime = jikan.top(type="anime", page=1, subtype="upcoming")
     id_list = get_id(top_anime)
-    shows = get_anime_object(id_list)
-    print_table(shows)
+    upcoming_shows = get_anime_object(id_list)
+
+    recommendations = recommended_list(our_list, upcoming_shows)
+
+    print('*************Watched Shows************')
+    print_table(our_list)
+
+    print('*************Upcoming Shows************')
+    print_table(upcoming_shows)
+
+    print('*************Recommended Based Off of Our List************')
+    print_table(recommendations)
 
 
 def get_id(data):
     anime_access = data['top']
     id_list = []
 
-    #Only use 25 of the 50 ID
+    # Only use 25 of the 50 ID
     for container in anime_access[:25]:
         identification = container['mal_id']
         id_list.append(identification)
@@ -29,9 +45,9 @@ def get_id(data):
 def get_anime_object(data):
     anime_list = []
 
-    #API RULES: 30 Requests/Minute
+    # API RULES: 30 Requests/Minute
     for id in data:
-        #API RULES: 4 Seconds between requests
+        # API RULES: 4 Seconds between requests
         time.sleep(4)
         show = jikan.anime(id)
         title = show['title']
@@ -51,12 +67,28 @@ def print_table(list):
         if current_length > max_length:
             max_length = current_length
 
-
     print("Title" + max_length*' ' + "Genre")
     spaces = 3 + max_length
     for anime in list:
         distance = spaces - len(anime['Title'])
         print(anime['Title'], distance*' ', anime['Genres'])
+
+
+def recommended_list(our_list, top_anime):
+    genre_list = []
+    for anime in our_list:
+        genre_list.append(anime['Genres'])
+
+    flattened_list = [genre for item in genre_list for genre in item]
+    flattened_list = list(dict.fromkeys(flattened_list))
+
+    recommendation = []
+    flattened_list = set(flattened_list)
+    for item in top_anime:
+        if set(item['Genres']) & set(flattened_list):
+            recommendation.append(item)
+
+    return recommendation
 
 
 main()
